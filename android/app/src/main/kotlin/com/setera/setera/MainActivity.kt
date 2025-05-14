977edb4dda7cf6a2827acd12d5052bddf4c20b36
Shayna <shaynaaltha30@gmail.com>
2025-05-14 14:19:46 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/setera/setera/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.setera.setera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/widgets/splash.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen ({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color (0xFF488DB4),
+      body: Center(
+        child: Container(
+          width: 158,
+          height: 131,
+          child: Image.asset("assets/logo/logo-splash.png"),
+        ),
+      ),
+    ),
+    );  
+  }
+}
